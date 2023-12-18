33cc5c380cccd7df5eb135ed74e5a86ac40177ac
Elua-b <bugingoeloi@gmail.com>
2023-12-18 09:41:23 +0200

    adding mock

--- src/test/java/rw/rca/devops/controller/MathControllerTest.java
+++ src/test/java/rw/rca/devops/controller/MathControllerTest.java
@@ -1,12 +1,19 @@
 package rw.rca.devops.controller;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import rw.rca.devops.service.MathService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MathService.class)
 public class MathControllerTest {
+    @MockBean
+    private MathService mathServiceMock;
 
+    @Autowired
+    private MockMvc mockMvc;
 }
