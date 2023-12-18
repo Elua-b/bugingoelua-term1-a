fe23bd0811f608e407e68aca461c6d4efced6a92
Elua-b <bugingoeloi@gmail.com>
2023-12-18 09:40:31 +0200

    adding imported packages

--- src/test/java/rw/rca/devops/controller/MathControllerTest.java
+++ src/test/java/rw/rca/devops/controller/MathControllerTest.java
@@ -1,4 +1,12 @@
 package rw.rca.devops.controller;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import rw.rca.devops.service.MathService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MathService.class)
 public class MathControllerTest {
+
 }
