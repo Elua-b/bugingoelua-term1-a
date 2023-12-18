3c5f89251fa0518b292f2f78afb79448555995ab
Elua-b <bugingoeloi@gmail.com>
2023-12-18 09:34:23 +0200

    adding content in controller

--- src/main/java/rw/rca/devops/controller/MathController.java
+++ src/main/java/rw/rca/devops/controller/MathController.java
@@ -1,10 +1,29 @@
 package rw.rca.devops.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rw.rca.devops.dto.DoMathRequest;
+import rw.rca.devops.service.MathService;
+import rw.rca.devops.utils.ApiResponse;
+import rw.rca.devops.utils.InvalidOperationException;
 
 @RestController
 @RequestMapping("/api/v1")
 
 public class MathController {
+    @Autowired
+    public static MathService mathService;
+
+    public MathController(MathService mathService) {
+        MathController.mathService = mathService;
+    }
+
+    @PostMapping("/doMath")
+    public ResponseEntity<?> doMath(@RequestBody DoMathRequest body) throws InvalidOperationException {
+        return ResponseEntity.ok(new ApiResponse(mathService.doMath(body.getOperand1(), body.getOperand2(), body.getOperation())));
+    }
 }
