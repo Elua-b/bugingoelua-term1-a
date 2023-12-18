85ab68209ff6e70e76ce7039b4fa93913be8f4bd
Elua-b <bugingoeloi@gmail.com>
2023-12-18 09:30:39 +0200

    making operations

--- src/main/java/rw/rca/devops/service/MathService.java
+++ src/main/java/rw/rca/devops/service/MathService.java
@@ -9,4 +9,15 @@ public class MathService {
         if ("/".equals(operation) && operand2 == (double) 0) {
             throw new InvalidOperationException("Cannot divide by 0");
         }
+        return switch (operation) {
+            case "*" -> operand1 * operand2;
+            case "/" -> operand1 / operand2;
+            case "+" -> operand1 + operand2;
+            case "-" -> operand1 - operand2;
+            case "**" -> Math.pow(operand1, operand2);
+            case "log" -> operand1 * Math.log10(operand2);
+            case "ln" -> operand1 * Math.log(operand2);
+            default -> throw new RuntimeException("Unknown operation");
+        };
+    }
 }
